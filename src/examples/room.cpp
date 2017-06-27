8c63819d81f6afb86ffcea3967579e616896d8f1
Ricardo <makiolo@gmail.com>
2017-06-27 12:58:01 +0200

    Update room.cpp

--- src/examples/room.cpp
+++ src/examples/room.cpp
@@ -6,56 +6,67 @@ HomieNode presence3("presence_3", "presence");
 HomieNode luxNode("lux_1", "lux");
 
 const int LUX_INTERVAL = 3  * 1000UL;
-const int PRESENCE_INTERVAL = 10;
+const int PRESENCE_INTERVAL = 60 * 1000UL;
 unsigned long lux_mark = 0;
 unsigned long presence_mark = 0;
+
+void send_presence(bool force = false)
+{
+  static bool _presence1 = false;
+  static bool _presence2 = false;
+  static bool _presence3 = false;
+
+  int val1 = digitalRead(D1);
+  int val2 = digitalRead(D2);
+  int val3 = digitalRead(D4);
+
+  bool p1 = (val1 == HIGH);
+  bool p2 = (val2 == HIGH);
+  bool p3 = (val3 == HIGH);
+
+  if(force || (_presence1 != p1))
+  {
+    presence1.setProperty("presence").send(p1 ? "true" : "false");
+    // HomieInternals::Interface::get().getSendingPromise().setNode(presence1).setProperty("presence").setQos(2).setRetained(true).overwriteSetter(false).setRange({ .isRange = false, .index = 0 }).send(p1 ? "true" : "false");
+    _presence1 = p1;
+  }
+
+  if(force || (_presence2 != p2))
+  {
+    presence2.setProperty("presence").send(p2 ? "true" : "false");
+    // HomieInternals::Interface::get().getSendingPromise().setNode(presence2).setProperty("presence").setQos(2).setRetained(true).overwriteSetter(false).setRange({ .isRange = false, .index = 0 }).send(p2 ? "true" : "false");
+    _presence2 = p2;
+  }
+
+  if(force || (_presence3 != p3))
+  {
+    presence3.setProperty("presence").send(p3 ? "true" : "false");
+    // HomieInternals::Interface::get().getSendingPromise().setNode(presence3).setProperty("presence").setQos(2).setRetained(true).overwriteSetter(false).setRange({ .isRange = false, .index = 0 }).send(p3 ? "true" : "false");
+    _presence3 = p3;
+  }
+}
+
+void setupHandler()
+{
+  send_presence(true);
+}
 
 void loopHandler()
 {
   if (millis() - lux_mark >= LUX_INTERVAL || lux_mark == 0)
   {
     int lux = analogRead(0);
-    Homie.getLogger() << "lux: " << lux << " °C" << endl;
+    Homie.getLogger() << "lux: " << lux << endl;
     luxNode.setProperty("lux").send(String(lux));
     lux_mark = millis();
   }
 
   if (millis() - presence_mark >= PRESENCE_INTERVAL || presence_mark == 0)
   {
-      static bool _presence1 = false;
-      static bool _presence2 = false;
-      static bool _presence3 = false;
-    
-      int val1 = digitalRead(D1);
-      int val2 = digitalRead(D2);
-      int val3 = digitalRead(D4);
-    
-      bool p1 = (val1 == HIGH);
-      bool p2 = (val2 == HIGH);
-      bool p3 = (val3 == HIGH);
-    
-      if(_presence1 != p1)
-      {
-        presence1.setProperty("presence").send(p1 ? "true" : "false");
-        // HomieInternals::Interface::get().getSendingPromise().setNode(presence1).setProperty("presence").setQos(2).setRetained(true).overwriteSetter(false).setRange({ .isRange = false, .index = 0 }).send(p1 ? "true" : "false");
-        _presence1 = p1;
-      }
-    
-      if(_presence2 != p2)
-      {
-        presence2.setProperty("presence").send(p2 ? "true" : "false");
-        // HomieInternals::Interface::get().getSendingPromise().setNode(presence2).setProperty("presence").setQos(2).setRetained(true).overwriteSetter(false).setRange({ .isRange = false, .index = 0 }).send(p2 ? "true" : "false");
-        _presence2 = p2;
-      }
-    
-      if(_presence3 != p3)
-      {
-        presence3.setProperty("presence").send(p3 ? "true" : "false");
-        // HomieInternals::Interface::get().getSendingPromise().setNode(presence3).setProperty("presence").setQos(2).setRetained(true).overwriteSetter(false).setRange({ .isRange = false, .index = 0 }).send(p3 ? "true" : "false");
-        _presence3 = p3;
-      }
+      send_presence(true);
       presence_mark = millis();
   }
+  send_presence();
 }
 
 void setup()
@@ -71,7 +82,7 @@ void setup()
   // Homie.disableLogging();
   Homie_setBrand("SalonDev");
   Homie_setFirmware("salon-dev", "0.0.5");
-  Homie.setLoopFunction(loopHandler);
+  Homie.setSetupFunction(setupHandler).setLoopFunction(loopHandler);
   Homie.setup(); 
 }
 
