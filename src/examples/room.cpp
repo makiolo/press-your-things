125658d2b13d78192895b38e531c0a54eb689ff5
Ricardo <makiolo@gmail.com>
2017-06-27 12:51:04 +0200

    Update room.cpp

--- src/examples/room.cpp
+++ src/examples/room.cpp
@@ -3,24 +3,24 @@
 HomieNode presence1("presence_1", "presence");
 HomieNode presence2("presence_2", "presence");
 HomieNode presence3("presence_3", "presence");
-HomieNode luxNode("lux", "lux");
+HomieNode luxNode("lux_1", "lux");
 
-const int TEMPERATURE_INTERVAL = 3  * 1000UL;
+const int LUX_INTERVAL = 3  * 1000UL;
 const int PRESENCE_INTERVAL = 10;
-unsigned long lastTemperatureSent = 0;
-unsigned long lastPresenceSent = 0;
+unsigned long lux_mark = 0;
+unsigned long presence_mark = 0;
 
 void loopHandler()
 {
-  if (millis() - lastTemperatureSent >= TEMPERATURE_INTERVAL || lastTemperatureSent == 0)
+  if (millis() - lux_mark >= LUX_INTERVAL || lux_mark == 0)
   {
-    int temperature = analogRead(0);
-    Homie.getLogger() << "lux: " << temperature << " °C" << endl;
-    luxNode.setProperty("lux").send(String(temperature));
-    lastTemperatureSent = millis();
+    int lux = analogRead(0);
+    Homie.getLogger() << "lux: " << lux << " °C" << endl;
+    luxNode.setProperty("lux").send(String(lux));
+    lux_mark = millis();
   }
 
-  if (millis() - lastPresenceSent >= PRESENCE_INTERVAL || lastPresenceSent == 0)
+  if (millis() - presence_mark >= PRESENCE_INTERVAL || presence_mark == 0)
   {
       static bool _presence1 = false;
       static bool _presence2 = false;
@@ -54,9 +54,8 @@ void loopHandler()
         // HomieInternals::Interface::get().getSendingPromise().setNode(presence3).setProperty("presence").setQos(2).setRetained(true).overwriteSetter(false).setRange({ .isRange = false, .index = 0 }).send(p3 ? "true" : "false");
         _presence3 = p3;
       }
-      lastTemperatureSent = millis();
+      presence_mark = millis();
   }
-
 }
 
 void setup()
