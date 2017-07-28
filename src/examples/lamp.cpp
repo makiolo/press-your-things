cce4de201982d0b50087d4a9af36be105f5abe2e
Ricardo <makiolo@gmail.com>
2017-07-28 18:43:30 +0200

    Update lamp.cpp

--- src/examples/lamp.cpp
+++ src/examples/lamp.cpp
@@ -1,7 +1,10 @@
 #include <Arduino.h>
 #include <Homie.h>
 
-/*
+#define FW_NAME "lamp"
+#define FW_VERSION "1.0.0"
+const char *__FLAGGED_FW_NAME = "\xbf\x84\xe4\x13\x54" FW_NAME "\x93\x44\x6b\xa7\x75";
+const char *__FLAGGED_FW_VERSION = "\x6a\x3f\x3e\x0e\xe1" FW_VERSION "\xb0\x30\x48\xd4\x1a";
 
 #if 0
 const int PIN_RELAY = 12;
@@ -149,4 +152,3 @@ void loop()
 {
   Homie.loop();
 }
-*/
