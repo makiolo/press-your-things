59e5e2b4949d211c5276a7c3ece5d3068c3409e8
Ricardo <makiolo@gmail.com>
2017-07-29 07:31:02 +0200

    Update main.cpp

--- button/main.cpp
+++ button/main.cpp
@@ -1,19 +1,14 @@
 #include <Arduino.h>
 #include <Homie.h>
-
-#define FW_NAME "button"
-#define FW_VERSION "1.0.0"
-const char *__FLAGGED_FW_NAME = "\xbf\x84\xe4\x13\x54" FW_NAME "\x93\x44\x6b\xa7\x75";
-const char *__FLAGGED_FW_VERSION = "\x6a\x3f\x3e\x0e\xe1" FW_VERSION "\xb0\x30\x48\xd4\x1a";
+#include <ArduinoOTA.h>
 
 #if 0
 const int PIN_RELAY = 12;
 const bool inverse_logic_relay = false;
 #else
 const int PIN_RELAY = 2;
-// const bool inverse_logic_relay = true;
-#endif
 const bool inverse_logic_relay = false;
+#endif
 
 const int PIN_BUTTON = 0;
 const int PIN_LED = 13;
@@ -25,40 +20,6 @@ unsigned long lastClickTime = 0;
 float catchTime = 0.5;
 
 HomieNode buttonNode("button", "button");
-
-void _press(bool on)
-{
-  if(inverse_logic_relay)
-  {
-    digitalWrite(PIN_RELAY, on ? LOW : HIGH);
-  }
-  else
-  {
-    digitalWrite(PIN_RELAY, on ? HIGH : LOW);
-  }
-  buttonNode.setProperty("on").send(on ? "true" : "false");
-  Homie.getLogger() << "Switch is " << (on ? "on" : "off") << endl;
-}
-
-void toggleRelay()
-{
-  if(inverse_logic_relay)
-  {
-    _press(digitalRead(PIN_RELAY) == HIGH);
-  }
-  else
-  {
-    _press(digitalRead(PIN_RELAY) == LOW);
-  }
-}
-
-void press_on() {
-  _press(true);
-}
-
-void press_off() {
-  _press(false);
-}
 
 // enviar estado a topic: homie/rele/button/on/set
 bool switchOnHandler(HomieRange range, String value)
@@ -79,6 +40,40 @@ bool switchOnHandler(HomieRange range, String value)
     return true;
   }
   return false;
+}
+
+void press_on() {
+  _press(true);
+}
+
+void press_off() {
+  _press(false);
+}
+
+void toggleRelay()
+{
+  if(inverse_logic_relay)
+  {
+    _press(digitalRead(PIN_RELAY) == HIGH);
+  }
+  else
+  {
+    _press(digitalRead(PIN_RELAY) == LOW);
+  }
+}
+
+void _press(bool on)
+{
+  if(inverse_logic_relay)
+  {
+    digitalWrite(PIN_RELAY, on ? LOW : HIGH);
+  }
+  else
+  {
+    digitalWrite(PIN_RELAY, on ? HIGH : LOW);
+  }
+  buttonNode.setProperty("on").send(on ? "true" : "false");
+  Homie.getLogger() << "Switch is " << (on ? "on" : "off") << endl;
 }
 
 void loopHandler_new()
@@ -139,16 +134,19 @@ void setup()
   // homie
   //Homie.setLedPin(PIN_LED, LOW).setResetTrigger(PIN_BUTTON, LOW, 5000);
   Homie.disableLedFeedback();
-  Homie.disableLogging();
-  Homie_setBrand(FW_NAME);
-  Homie_setFirmware(FW_NAME, FW_VERSION);
+  //Homie.disableLogging();
+  Homie_setBrand("button");
+  Homie_setFirmware("button", "1.0.3");
   Homie.setLoopFunction(loopHandler);
   buttonNode.advertise("on").settable(switchOnHandler);
   Homie.setup();
-  press_on();
+  ArduinoOTA.setHostname(Homie.getConfiguration().deviceId);
+  ArduinoOTA.begin();
+  // press_on();
 }
 
 void loop()
 {
   Homie.loop();
+  ArduinoOTA.handle();
 }
