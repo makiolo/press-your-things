f403b6713b03d164647aa56a131ea919db50fd10
Ricardo <makiolo@gmail.com>
2017-08-06 02:47:36 +0200

    Update main.cpp

--- presence/main.cpp
+++ presence/main.cpp
@@ -3,7 +3,7 @@
 #define FW_NAME "presence"
 #define FW_VERSION "1.0.0"
 const char *__FLAGGED_FW_NAME = "\xbf\x84\xe4\x13\x54" FW_NAME "\x93\x44\x6b\xa7\x75";
-const char *__FLAGGED_FW_VERSION = "\x6a\x3f\x3e\x0e\xe1" FW_VERSION "\xb0\x30\x48\xD4\x1a";
+const char *__FLAGGED_FW_VERSION = "\x6a\x3f\x3e\x0e\xe1" FW_VERSION "\xb0\x30\x48\xd4\x1a";
 
 HomieNode presence1("presence_1", "presence");
 HomieNode presence2("presence_2", "presence");
