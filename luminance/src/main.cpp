2dd13f4e55f1b66389c72e0eea1d2fc5018bb136
Ricardo <makiolo@gmail.com>
2017-10-27 00:00:09 +0200

    Update main.cpp

--- luminance/src/main.cpp
+++ luminance/src/main.cpp
@@ -1,6 +1,11 @@
 #include <Wire.h>
 #include "TSL2561.h"
 
+#define FW_NAME "luminance"
+#define FW_VERSION "1.0.0"
+const char *__FLAGGED_FW_NAME = "\xbf\x84\xe4\x13\x54" FW_NAME "\x93\x44\x6b\xa7\x75";
+const char *__FLAGGED_FW_VERSION = "\x6a\x3f\x3e\x0e\xe1" FW_VERSION "\xb0\x30\x48\xd4\x1a";
+
 // Example for demonstrating the TSL2561 library - public domain!
 
 // connect SCL to analog 5 (D1 in nodemcu)
@@ -13,6 +18,7 @@
 // The address will be different depending on whether you let
 // the ADDR pin float (addr 0x39), or tie it to ground or vcc. In those cases
 // use TSL2561_ADDR_LOW (0x29) or TSL2561_ADDR_HIGH (0x49) respectively
+
 TSL2561 tsl(TSL2561_ADDR_FLOAT); 
 
 void setup(void)
