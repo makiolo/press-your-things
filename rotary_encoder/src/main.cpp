cdcc783b6983030e5c79336c6ff4aacda9301ac7
Ricardo <makiolo@gmail.com>
2017-10-26 23:58:37 +0200

    Update main.cpp

--- rotary_encoder/src/main.cpp
+++ rotary_encoder/src/main.cpp
@@ -1,4 +1,10 @@
 #include <Arduino.h>
+
+#define FW_NAME "rotary_encoder"
+#define FW_VERSION "1.0.0"
+const char *__FLAGGED_FW_NAME = "\xbf\x84\xe4\x13\x54" FW_NAME "\x93\x44\x6b\xa7\x75";
+const char *__FLAGGED_FW_VERSION = "\x6a\x3f\x3e\x0e\xe1" FW_VERSION "\xb0\x30\x48\xd4\x1a";
+
 int pinCLK = D7;
 int pinDT = D6;
 int pinBUTTON = D5;
