b1932b708969c62b20e9ccf98a49169bd9ac6e82
Ricardo <makiolo@gmail.com>
2017-10-27 00:00:41 +0200

    Update main.cpp

--- temperature/src/main.cpp
+++ temperature/src/main.cpp
@@ -1,4 +1,10 @@
 #include <DHT.h>
+
+#define FW_NAME "temperature"
+#define FW_VERSION "1.0.0"
+const char *__FLAGGED_FW_NAME = "\xbf\x84\xe4\x13\x54" FW_NAME "\x93\x44\x6b\xa7\x75";
+const char *__FLAGGED_FW_VERSION = "\x6a\x3f\x3e\x0e\xe1" FW_VERSION "\xb0\x30\x48\xd4\x1a";
+
 DHT dht(D3, DHT22);
 
 void setup(void)
@@ -28,4 +34,3 @@ void loop(void)
 	Serial.println(hic);
 	delay(100); 
 }
-
