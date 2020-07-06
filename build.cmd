ecf994f507b2071031444e865ea5659039703f49
Ricardo <makiolo@gmail.com>
2020-07-06 22:43:16 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,5 +1,6 @@
 @echo off
 :: esptool.py --port COM1 erase_flash
+:: esptool.py -cp COM1 -ce -v
 chgport /QUERY
 :: C:\python27\scripts\platformio platform update
 C:\python27\scripts\platformio run -d %1 --upload-port %2 -t upload
